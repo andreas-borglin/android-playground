98c2d5790ce9ebb9e55daef41bdf48e9854d32fe
Andreas Borglin <andreas@jakoon.co.uk>
2019-05-15 11:55:40 +0100

    Added Retrofit service using coroutines and defined API models

---
+++ app/src/main/java/com/jakoon/babylon/api/ApiModels.kt
@@ -0,0 +1,22 @@
+package com.jakoon.babylon.api
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Post(val id: Int, val userId: Int, val title: String, val body: String)
+
+@JsonClass(generateAdapter = true)
+data class User(
+    val id: Int, val name: String, val username: String, val email: String, val address: Address,
+    val phone: String, val website: String, val company: Company
+)
+
+@JsonClass(generateAdapter = true)
+data class Comment(val id: Int, val postId: Int, val name: String, val email: String, val body: String)
+
+data class Geo(val lat: String, val lng: String)
+
+data class Address(val street: String, val suite: String, val city: String, val zipcode: String, val geo: Geo)
+
+data class Company(val name: String, val catchPhrase: String, val bs: String)
+
---
+++ app/src/main/java/com/jakoon/babylon/api/TypicodeJsonApi.kt
@@ -0,0 +1,16 @@
+package com.jakoon.babylon.api
+
+import retrofit2.http.GET
+
+interface TypicodeJsonApi {
+
+    @GET("posts")
+    suspend fun getPosts(): List<Post>
+
+    @GET("users")
+    suspend fun getUsers(): List<User>
+
+    @GET("comments")
+    suspend fun getComments(): List<Comment>
+
+}
